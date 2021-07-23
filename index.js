91954b622dbcf88466d17d34ebbbd84ae854bddb
Henry Park <henry@terra.money>
2021-07-23 13:01:50 +0900

    Fix Xcode 12.5 compatibility

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry } from 'react-native'
 import 'node-libs-react-native/globals'
+import 'react-native-get-random-values'
 import App from './App'
 import { name as appName } from './app.json'
 
